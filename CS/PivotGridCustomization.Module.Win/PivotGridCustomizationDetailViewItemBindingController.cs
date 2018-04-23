310687a79dee5e78a8349579dd7ec3216f703a75
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/PivotGridCustomization.Module.Win/PivotGridCustomizationDetailViewItemBindingController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.PivotChart;
+using DevExpress.ExpressApp.PivotChart.Win;
+
+namespace PivotGridCustomization.Module.Win {
+    public class PivotGridCustomizationDetailViewItemBindingController : AnalysisViewControllerBase {
+        private PivotGridCustomizationDetailViewItem FindPivotGridCustomizationItem() {
+            DetailView view = (DetailView)View;
+            IList<PivotGridCustomizationDetailViewItem> items = view.GetItems<PivotGridCustomizationDetailViewItem>();
+            if (items.Count != 0) {
+                return items[0];
+            }
+            return null;
+        }
+        private void BindPivotGridCustomizationItemToEditor(AnalysisEditorWin analysisEditor) {
+            PivotGridCustomizationDetailViewItem customizationItem = FindPivotGridCustomizationItem();
+            if (customizationItem != null) {
+                customizationItem.SetAnalysisEditor(analysisEditor);
+            }
+        }
+        protected override void OnAnalysisControlCreated() {
+            base.OnAnalysisControlCreated();
+            BindPivotGridCustomizationItemToEditor((AnalysisEditorWin)analysisEditor);
+        }
+        protected override void OnDeactivating() {
+            BindPivotGridCustomizationItemToEditor(null);
+            base.OnDeactivating();
+        }
+    }
+}
---
+++ VB/PivotGridCustomization.Module.Win/WinModule.vb
@@ -0,0 +1,22 @@
+Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Text
+Imports System.ComponentModel
+
+Imports DevExpress.ExpressApp
+Imports DevExpress.ExpressApp.Editors
+
+Namespace PivotGridCustomization.Module.Win
+	<ToolboxItemFilter("Xaf.Platform.Win")> _
+	Public NotInheritable Partial Class PivotGridCustomizationWindowsFormsModule
+		Inherits ModuleBase
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+                                 Protected Overrides Sub RegisterEditorDescriptors(ByVal editorDescriptors As List(Of DevExpress.ExpressApp.Editors.EditorDescriptor))
+			MyBase.RegisterEditorDescriptors(editorDescriptors)
+			editorDescriptors.Add(New DetailViewItemDescriptor(New DetailViewItemRegistration(GetType(IModelPivotGridCustomizationItem), GetType(PivotGridCustomizationDetailViewItem), True)))
+                                  End Sub
+	End Class
+End Namespace
